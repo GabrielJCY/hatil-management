64171e99baf0558257e0242b6dcbd72121a94101
Gabriel JCY <gabitoyody@gmail.com>
2026-01-09 22:52:57 -1200

    Corrijo index.php para Render gratis, usa ->environment correctamente

--- public/index.php
+++ public/index.php
@@ -5,15 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
@@ -21,26 +18,18 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |--------------------------------------------------------------------------
 | Run Migrations Automatically (Render Free Plan)
 |--------------------------------------------------------------------------
-|
-| En Render gratis no podemos usar la consola para ejecutar migraciones.
-| Esta sección ejecuta automáticamente las migraciones pendientes al
-| iniciar la aplicación. Solo recomendable para demos o portafolios.
-|
 */
 if ($app->environment('production')) {
     try {
-        // Ejecuta migraciones pendientes
         \Artisan::call('migrate', ['--force' => true]);
     } catch (\Exception $e) {
         \Log::error('Error migrando DB: ' . $e->getMessage());
     }
 
-    // Limpiar y regenerar caches de Laravel
     \Artisan::call('config:cache');
     \Artisan::call('route:cache');
     \Artisan::call('view:cache');
 }
 
-// Handle the request and send the response...
 $response = $app->handle(Request::capture());
 $response->send();
